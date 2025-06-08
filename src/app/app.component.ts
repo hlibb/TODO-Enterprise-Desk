065e1d9d2250083997b9b808f4dfa7c08c9cdce6
Hlib Astashev <astashev01@gmail.com>
2025-06-08 11:10:55 +0200

    30. Defining Component Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";;
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,5 @@ import { UserComponent } from "./user/user.component";;
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'max-schwarzmueller-course';
+  users = DUMMY_USERS;
 }
