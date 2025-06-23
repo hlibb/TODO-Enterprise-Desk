3a796cf82dd0d1fdaf3db075db4810973dac8cdd
Hlib Astashev <astashev01@gmail.com>
2025-06-23 07:25:53 +0200

    fallback on non-signal approach

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,17 +7,12 @@ import { Component, Input, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-//  @Input({required: true}) avatar!: string;
-//  @Input({required: true}) name!: string; 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
-//  get imagePath() {
-//    return 'assets/users/' + this.avatar;
-//  }
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string; 
+  
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
 
