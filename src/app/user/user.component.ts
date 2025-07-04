635773b74d7f75a18f11f7cb1efb2af48b94505f
Hlib Astashev <astashev01@gmail.com>
2025-07-04 09:12:20 +0200

    fallback on old approach with decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,8 +10,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string; 
-  //@Output() select = new EventEmitter();
-  select = output<string>(); // auto creation of new EventEmitter()
+  @Output() select = new EventEmitter();
   
   get imagePath() {
     return 'assets/users/' + this.avatar;
